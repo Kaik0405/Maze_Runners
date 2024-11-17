8570e313813fba674d103d4fe59737b325e12076
Kaik0405 <159601515+Kaik0405@users.noreply.github.com>
2024-11-17 13:04:28 -0500

    FinishLine Ready

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -9,9 +9,18 @@ public class Cell
     public bool cellTeleport{get;set;}
     public bool Start{get;set;}
     public bool FinishLine{get;set;}
+    public TrampType trampType = TrampType.Unknown;
 
     public Cell(bool Obstacle)
     {
         this.Obstacle = Obstacle;  
     } 
+}
+public enum TrampType{
+    returnInit, //mueve la ficha para la salida
+    Freeze,     //evita que la ficha se mueva mas
+    Sleep,      //evita que la ficha se mueva por 2 turnos
+    Spines,     //hace que la ficha se mueva la mitad de los pasos restantes
+    Unknown
+
 }
--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 using Unity.VisualScripting;
 using UnityEngine;
 using System.Security;
+using Unity.Mathematics;
 
 public class MazeGenerator : MonoBehaviour
 {
@@ -18,7 +19,7 @@ public class MazeGenerator : MonoBehaviour
     const bool Wall = true; //representa una pared
     const bool Path = false; //representa un camino
     public static Cell[,] Maze = new Cell[Height + 2, Width + 2]; 
-    struct Cells
+    private struct Cells
     {
         public int x, y;
         public Cells(int x, int y)
@@ -34,18 +35,19 @@ public class MazeGenerator : MonoBehaviour
         CellObject = CellObjectScene;
         CellImage = CellObject.GetComponent<Image>();
         GenerateMazeInScene();
+
     }
-    void CreateMatrix()
+    private void CreateMatrix() //Metodo para generar una matriz de celdas clasificadas como obstaculos
     {
         for (int i = 0;i<Maze.GetLength(0);i++)
             for (int j = 0;j<Maze.GetLength(1);j++)    
                 Maze[i,j] = new Cell(Wall);        
     }    
-    bool IsInBounds(int x, int y) // Método para verificar si una celda esta en una posición valida de la matriz
+    private bool IsInBounds(int x, int y) // Método para verificar si una celda esta en una posición valida de la matriz
     {
         return x >= 0 && x < Maze.GetLength(0) && y >= 0 && y < Maze.GetLength(1); // Ajusta los limites
     }
-    void Shuffle(Cells[] directions) //Selecciona las celdas en el Radio 2 de la celda actual y las mezcla de forma aleatoria 
+    private void Shuffle(Cells[] directions) //Selecciona las celdas en el Radio 2 de la celda actual y las mezcla de forma aleatoria 
     {
         System.Random rand = new System.Random();
         for (int i = 0; i < directions.Length; i++)
@@ -56,7 +58,7 @@ public class MazeGenerator : MonoBehaviour
             directions[j] = temp;
         }
     }
-    void GenerateMaze(int x,int y) // Algoritmo recursivo para generar el laberinto
+    private void GenerateMaze(int x,int y) // Algoritmo recursivo para generar el laberinto
     {
         Maze[x, y].Obstacle = Path;
 
@@ -82,14 +84,14 @@ public class MazeGenerator : MonoBehaviour
             }
         }
     }
-    bool Verification(int x, int y, int li, int lj) //Metodo para verificar si la matriz esta en rango
+    private bool Verification(int x, int y, int li, int lj) //Metodo para verificar si la matriz esta en rango
     {
         if (x < 0 || y < 0) return false;
         if (x > li || y > lj) return false;
 
         return true;
     }
-    bool[,] CopyValues(bool[,] matrix)
+    private bool[,] CopyValues(bool[,] matrix) // metodo para hacer una copia de la matriz original
     {
         for (int i = 0;i<matrix.GetLength(0);i++)        
             for(int j = 0;j<matrix.GetLength(1);j++)
@@ -97,15 +99,18 @@ public class MazeGenerator : MonoBehaviour
 
         return matrix;    
     }
-    Cells LongestPath(Cell[,] maze) // código recursivo para determinar el camino mas largo del laberinto
+    private Cells LongestPath(Cell[,] maze) // código recursivo para determinar el camino mas largo del laberinto
     {
         int max = 0;
         Cells cellF = new Cells(0,0);
         bool[,] mazeAux = new bool[Maze.GetLength(0), Maze.GetLength(1)];
+        
         CopyValues(mazeAux);
+        LongestPath(mazeAux,ref cellF,ref max);
+
         return cellF;
     } 
-    void LongestPath(bool[,] booleanMask,ref Cells cellF,ref int max,int currentMax = 0,int x = 1,int y = 1) 
+    private void LongestPath(bool[,] booleanMask,ref Cells cellF,ref int max,int currentMax = 0,int x = 1,int y = 1) 
     {
         int[] dx = { 0, 1, 0, -1 };
         int[] dy = { 1, 0, -1, 0 };
@@ -115,19 +120,47 @@ public class MazeGenerator : MonoBehaviour
             int sumX = x + dx[s];
             int sumY = y + dy[s];
 
-            
+            if(Verification(sumX,sumY,booleanMask.GetLength(0),booleanMask.GetLength(1)) && !booleanMask[sumX,sumY])
+            {
+                if(!booleanMask[sumX,sumY])
+                {
+                    booleanMask[sumX,sumY]=true;
+                    LongestPath(booleanMask,ref cellF,ref max,currentMax + 1,sumX,sumY);
+                    booleanMask[sumX,sumY]=false;
+                }
+            }
+        }
+        if(currentMax > max)
+        {
+            max = currentMax;
+            cellF.x = x;
+            cellF.y = y;
         }
     }
-    void GenerateMazeInScene()
+    private void GenerateMazeInScene() //instanciacion en escena de las celdas del laberinto
     {
+        Maze[1,1].Start = true;
+        Cells finish = LongestPath(Maze);
+        Maze[finish.x,finish.y].FinishLine = true;
+
         for (int i = 0; i < Maze.GetLength(0); i++)
         {
             for (int j = 0; j < Maze.GetLength(1); j++)
             {
-                if((Maze[i,j] != null)&&(Maze[i,j].Obstacle == true))
+                if(Maze[i,j].Obstacle == true)
                 {
                     CellImage.color = Color.grey;
                     Instantiate(CellObject,CellObject.transform.position,CellObject.transform.rotation);    
+                }
+                else if(Maze[i,j].Start == true)
+                {
+                    CellImage.color = Color.blue;
+                    Instantiate(CellObject,CellObject.transform.position,CellObject.transform.rotation);
+                }
+                else if(Maze[i,j].FinishLine == true)
+                {
+                    CellImage.color = Color.red;
+                    Instantiate(CellObject,CellObject.transform.position,CellObject.transform.rotation);
                 }
                 else
                 {
@@ -137,5 +170,12 @@ public class MazeGenerator : MonoBehaviour
             }
         }
     }
-    
+    public void GenerateTramps() // metodo para generar las trampas de forma aleatoria
+    {
+        //type code here....
+    }
+    public void GenerateTeleports() // metodo para generar las zonas de teletransporte 
+    {
+        //type code here....
+    }
 }
